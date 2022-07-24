7c50212c939d14e1f6db87bbde38ad03996503cc
KonaDaniel <79358753+KonaDaniel@users.noreply.github.com>
2022-07-24 10:13:03 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+status = "";
+object = "";
+
+function setup()
+{
+    canvas = createCanvas(500, 400);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+    video.size(480, 380);
+}
+
+function start()
+{
+    objectDetector = ml5.objectDetector("cocossd", modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+    object = document.getElementById("input_object_name").value;
+}
+
+function modelLoaded()
+{
+    console.log("Model is Ready!");
+    status = "true";
+}
+
+function draw()
+{
+   image(video, 0, 0, 500, 400);
+}
